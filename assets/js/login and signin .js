77754df6b7f5fb1897e2f0690154f3eb632b165c
Tamilselvan2747 <116268684+Tamilselvan2747@users.noreply.github.com>
2023-03-13 15:42:03 +0530

    update

--- assets/js/login and signin .js
+++ assets/js/login and signin .js
@@ -1,9 +1,42 @@
 
  
  function signUp(e){
+    e.preventDefault();  
+
     let name = document.getElementById('name').value,
         email = document.getElementById('email').value,
         password = document.getElementById('password').value;
+        confirm_password = document.getElementById('confirm_password').value;
+        // unqiue_id =  unqiue_id;
+        phone_number= "";
+        date_of_birth = "";
+        gender= "";
+        blood_group= "";
+        street_name= "";
+        colony="";
+        city="";
+        state="";
+        country="";
+        pincode="";
+        drug="";
+        disease="";
+        operation_dates="";
+        medications="";
+        let unqiue_id = uuidv4();
+
+    //    errorMessage = document.getElementById("errorMessage");
+
+    function checkPassword(){
+        if (password!==confirm_password){
+            alert("Re-enter the Password");
+            confirm_password.value=reset();
+        }
+        else{
+            alert("Password Matched");
+        }
+    }
+    checkPassword();
+
 
     let formData = JSON.parse(localStorage.getItem('formData')) || [];
 
@@ -13,18 +46,25 @@
          );
 
     if(!exist){
-        formData.push({ name, email, password });
+        formData.push({ name, email, password, phone_number, date_of_birth, blood_group, street_name, colony, city, state, pincode, country, drug, disease, operation_dates, medications,unqiue_id  });
         localStorage.setItem('formData', JSON.stringify(formData));
         document.querySelector('form').reset();
+
         alert("Account Created.\n\nPlease Sign In using the link below.");
     }
     else{
         alert("ALREADY EXISTED!!!\nYou have already signed up");
     }
-    e.preventDefault();
+
 }
+  
+
+
+
 
 function signIn (e) {
+    e.preventDefault();
+
     let name = document.getElementById('name').value, 
         password = document.getElementById('password').value;
 
@@ -32,8 +72,8 @@ function signIn (e) {
 
     let exist = formData.length && 
     JSON.parse(localStorage.getItem('formData')).some(data => 
-        data.name.toLowerCase() == name && 
-        data.password.toLowerCase() == password);
+        data.name == name && 
+        data.password == password);
 
     if(!exist){
         alert("Incorrect login credentials");
@@ -41,8 +81,28 @@ function signIn (e) {
 
     }
     else{
-        location.href = "../../index.html"
+         localStorage.setItem("name_id",JSON.stringify (name));
+        location.href = "../products/profile.html"  
 
     }
-    e.preventDefault();
+}
+
+
+
+
+
+    function validatePassword() {
+        if (password.value === "") {
+          alert ("Password cannot be empty");
+          return false;
+        } else if (password.value.length < 8) {
+          alert ("Password must be at least 8 characters long");
+          return false;
+        } else if (password.value !== confirmPassword.value) {
+         alert("Passwords do not match");
+          return false;
+        } else {
+         alert("");
+          return true;
+        }
 }
