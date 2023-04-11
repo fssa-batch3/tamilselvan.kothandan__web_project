567e6d6da49a2c62b95a8139d59f36fa49001bbb
Tamilselvan2747 <116268684+Tamilselvan2747@users.noreply.github.com>
2023-04-11 18:36:08 +0530

    bug fixing

--- assets/js/login and signin .js
+++ assets/js/login and signin .js
@@ -1,38 +1,38 @@
 
- 
- function signUp(e){
-    e.preventDefault();  
+
+function signUp(e) {
+    e.preventDefault();
 
     let name = document.getElementById('name').value,
         email = document.getElementById('email').value,
         password = document.getElementById('password').value;
-        confirm_password = document.getElementById('confirm_password').value;
-        // unqiue_id =  unqiue_id;
-        let unqiue_id = uuidv4();
-        phone_number= "";
-        date_of_birth = "";
-        gender= "";
-        blood_group= "";
-        street_name= "";
-        colony="";
-        city="";
-        state="";
-        country="";
-        pincode="";
-        drug="";
-        disease="";
-        operation_dates="";
-        medications="";
-         
+    confirm_password = document.getElementById('confirm_password').value;
+    // unqiue_id =  unqiue_id;
+    let unqiue_id = uuidv4();
+    phone_number = "";
+    date_of_birth = "";
+    gender = "";
+    blood_group = "";
+    street_name = "";
+    colony = "";
+    city = "";
+    state = "";
+    country = "";
+    pincode = "";
+    drug = "";
+    disease = "";
+    operation_dates = "";
+    medications = "";
+
 
     //    errorMessage = document.getElementById("errorMessage");
 
-    function checkPassword(){
-        if (password!==confirm_password){
+    function checkPassword() {
+        if (password !== confirm_password) {
             alert("Re-enter the Password");
-            confirm_password.value=reset();
+            confirm_password.value = reset();
         }
-        else{
+        else {
             alert("Password Matched");
         }
     }
@@ -41,49 +41,49 @@
 
     let formData = JSON.parse(localStorage.getItem('formData')) || [];
 
-    let exist = formData.length && 
-        JSON.parse(localStorage.getItem('formData')).some(data => 
-            data.name.toLowerCase() == name.toLowerCase() 
-         );
+    let exist = formData.length &&
+        JSON.parse(localStorage.getItem('formData')).some(data =>
+            data.name.toLowerCase() == name.toLowerCase()
+        );
 
-    if(!exist){
-        formData.push({ name, email, password, phone_number, date_of_birth, blood_group, street_name, colony, city, state, pincode, country, drug, disease, operation_dates, medications,unqiue_id  });
+    if (!exist) {
+        formData.push({ name, email, password, phone_number, date_of_birth, blood_group, street_name, colony, city, state, pincode, country, drug, disease, operation_dates, medications, unqiue_id });
         localStorage.setItem('formData', JSON.stringify(formData));
         document.querySelector('form').reset();
 
         alert("Account Created.\n\nPlease Sign In using the link below.");
     }
-    else{
+    else {
         alert("ALREADY EXISTED!!!\nYou have already signed up");
     }
 
 }
-  
 
 
 
 
-function signIn (e) {
+
+function signIn(e) {
     e.preventDefault();
 
-    let name = document.getElementById('name').value, 
+    let name = document.getElementById('name').value,
         password = document.getElementById('password').value;
 
     let formData = JSON.parse(localStorage.getItem('formData')) || [];
 
-    let exist = formData.length && 
-    JSON.parse(localStorage.getItem('formData')).some(data => 
-        data.name == name && 
-        data.password == password);
+    let exist = formData.length &&
+        JSON.parse(localStorage.getItem('formData')).some(data =>
+            data.name == name &&
+            data.password == password);
 
-    if(!exist){
+    if (!exist) {
         alert("Incorrect login credentials");
 
 
     }
-    else{
-         localStorage.setItem("name_id",JSON.stringify (name));
-        location.href = "../products/profile.html"  
+    else {
+        localStorage.setItem("name_id", JSON.stringify(name));
+        location.href = "../products/profile.html"
 
     }
 }
@@ -92,18 +92,18 @@ function signIn (e) {
 
 
 
-    function validatePassword() {
-        if (password.value === "") {
-          alert ("Password cannot be empty");
-          return false;
-        } else if (password.value.length < 8) {
-          alert ("Password must be at least 8 characters long");
-          return false;
-        } else if (password.value !== confirmPassword.value) {
-         alert("Passwords do not match");
-          return false;
-        } else {
-         alert("");
-          return true;
-        }
+function validatePassword() {
+    if (password.value === "") {
+        alert("Password cannot be empty");
+        return false;
+    } else if (password.value.length < 8) {
+        alert("Password must be at least 8 characters long");
+        return false;
+    } else if (password.value !== confirmPassword.value) {
+        alert("Passwords do not match");
+        return false;
+    } else {
+        alert("");
+        return true;
+    }
 }
