bf3cd89c6b858b66ad650b8537397ea6d67cb6d6
Tamilselvan2747 <116268684+Tamilselvan2747@users.noreply.github.com>
2023-04-14 20:23:13 +0530

    adding features

--- Components/header.js
+++ Components/header.js
@@ -2,13 +2,48 @@
 
 
 const beforeLogin = `
+                    <header class="header">
+
+<a href="../../index.html" class="logo"> Medical service </a>
+
+<nav class="navbar">
+  <a href="../../index.html">Home</a>
+  <a href="../../index.html">services</a>
+  <a href="./About page.html">About</a>
+  <a href="./login.html">Login</a>
+</nav>
 
 
 `
 
 const afterLogin = `
+<header class="header">
+
+        <a href="./index.html" class="logo"><i class="fa fa-heart" style="font-size:28px;color:#0e6453"></i> Medical
+            Service </a>
+
+        <nav class="navbar" id="navbar">
+            <a href="./index.html">Home</a>
+            <a href="#services ">Services</a>
+            <a href="./pages/products/About page.html">About</a>
+            <a href="./pages/products/Doctors.html">Doctors</a>
+            <a href="./pages/products/hospital.html">Hospital</a>
+            <a href="./pages/products/login.html">Login</a>
 
 
+            <div class="dropdown">
+                <button class="dropbtn"><i class='fas fa-user-alt' style='font-size:18px;color:#0e6453'></i></button>
+                <div class="dropdown-content">
+                    <a href="./pages/products/profile.html">View profile</a>
+                    <a href="./pages/products/past appointment.html">My appointment</a>
+                    <a href="#">Logout</a>
+                </div>
+            </div>
+        </nav>
+
+        <div id="menu-btn" class="fas fa-bars"></div>
+
+    </header>
 
 `
 
