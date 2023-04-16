b27d35260ecd6e0ce075319661a562ba6a2e38c4
Tamilselvan2747 <116268684+Tamilselvan2747@users.noreply.github.com>
2023-04-16 08:39:20 +0530

    fixing some buges

--- admin/logout.js
+++ admin/logout.js
@@ -1,9 +1,19 @@
-document.getElementById("logout_link").addEventListener("click", function (event) {
-    event.preventDefault();
-    logout();
-});
 
-function logout() {
-    localStorage.clear();
+
+
+function logout(unique_id) {
+    let admin = JSON.parse(localStorage.getItem('admin')) || [];
+
+    // Find the index of the user in the admin array
+    let index = admin.findIndex(admin => admin.unique_id === unique_id);
+
+    if (index !== -1) {
+        // Remove the user from the admin array
+        admin.splice(index, 1);
+        localStorage.setItem('admin', JSON.stringify(admin));
+    }
+    // Redirect the user to the login page or homepage
     window.location.href = "../index.html";
 }
+
+
